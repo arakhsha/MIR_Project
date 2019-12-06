94f599622cba814df73392f3ae6ec50f433c1bf7
aminrakhsha <a.rakhsha144@gmail.com>
2019-12-06 16:44:58 +0330

    ADD reading csv

--- src/data_extaction.py
+++ src/data_extaction.py
@@ -18,15 +18,19 @@ def extract_xml(filename):
     docs = []
     for page in root.findall("{http://www.mediawiki.org/xml/export-0.10/}page"):
         id = page.find("{http://www.mediawiki.org/xml/export-0.10/}id").text
-        text = page.find("{http://www.mediawiki.org/xml/export-0.10/}revision")\
-                    .find("{http://www.mediawiki.org/xml/export-0.10/}text").text
+        text = page.find("{http://www.mediawiki.org/xml/export-0.10/}revision") \
+            .find("{http://www.mediawiki.org/xml/export-0.10/}text").text
         docs.append(Doc(id, text))
     return docs
 
 
 def extract_csv(filename):
-    data = pd.read_csv(filename)
-    print(data.head(10))
+    docs = []
+    df = pd.read_csv(filename)
+    for i in range(df.shape[0]):
+        text = df['Title'].values[i] + " " + df['Text'].values[i]
+        docs.append(Doc(i, text))
+    return docs
 
 
 def read_docs(filename):
@@ -36,7 +40,7 @@ def read_docs(filename):
 
 
 if __name__ == "__main__":
-    docs = read_docs("../data/Persian.xml")
-    print(docs[0].id, docs[0].text)
+    # docs = read_docs("../data/Persian.xml")
+    docs = read_docs("../data/English.csv")
 
-    # docs = read_docs("../data/English.csv")
+    print(docs[0].id, docs[0].text)
