aa5399278917f3b78c151f114add1eb663c38782
mahbodmajid <mahbod.majid@gmail.com>
2019-12-06 20:33:08 +0330

    ADD independent stopping words finder

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,62 +1,60 @@
 import nltk
 import string
 import re
-
 from data_extaction import read_docs
 
 stemmer = nltk.PorterStemmer()
 
 
-def frequency_table(tokens, n=0):
-    import collections
-    counter = collections.Counter(tokens)
-    if n == 0:
-        n = len(tokens)
-    return counter.most_common(n)
-
-
-def normalize(text):
-    return text.lower()
-
-
-def tokenize(text):
-    return nltk.tokenize.word_tokenize(text)
-
-
-def remove_punctuation(tokens):
-    return [t for t in tokens if t not in string.punctuation]
-
-
-def remove_stop_words(tokens, stopping_words):
-    return [x for x in tokens if x not in stopping_words]
-
-
-def find_stop_words(filename, percent=0.0015):
+def find_stop_words(self, filename, percent=0.0015):
     docs = read_docs(filename)
     fulltext = ''
     for doc in docs:
         fulltext += doc.text
-    tokens = remove_punctuation(tokenize(normalize(fulltext)))
-    ft = frequency_table(tokens)
-    return [x for x,y in ft if y > percent*len(tokens)]
+    tokens = self.remove_punctuation(self.tokenize(self.normalize(fulltext)))
+    ft = self.frequency_table(tokens)
+    return [x for x, y in ft if y > percent * len(tokens)]
 
 
-def filter_tokens(tokens, min_size=0, special_chars=False):
-    if min_size > 0:
-        tokens = [t for t in tokens if len(t) >= min_size]
-    if special_chars:
-        tokens = [t for t in tokens if re.search('[^a-zA-Z-]', t) is None]
-    return tokens
+class EnglishPreprocessor:
+    stopping_words = []
 
+    def set_stop_words(self, filename):
+        self.stopping_words = find_stop_words(filename)
 
-def stem(tokens):
-    return [stemmer.stem(t) for t in tokens]
+    def frequency_table(self, tokens, n=0):
+        import collections
+        counter = collections.Counter(tokens)
+        if n == 0:
+            n = len(tokens)
+        return counter.most_common(n)
 
+    def normalize(self, text):
+        return text.lower()
 
-def preprocess(text, stopping_words):
-    tokens = remove_punctuation(tokenize(normalize(text)))
-    stems = stem(remove_stop_words(tokens, stopping_words))
-    return filter_tokens(stems)
+    def tokenize(self, text):
+        return nltk.tokenize.word_tokenize(text)
+
+    def remove_punctuation(self, tokens):
+        return [t for t in tokens if t not in string.punctuation]
+
+    def remove_stop_words(self, tokens):
+        return [x for x in tokens if x not in self.stopping_words]
+
+    def filter_tokens(self, tokens, min_size=0, special_chars=False):
+        if min_size > 0:
+            tokens = [t for t in tokens if len(t) >= min_size]
+        if special_chars:
+            tokens = [t for t in tokens if re.search('[^a-zA-Z-]', t) is None]
+        return tokens
+
+    def stem(self, tokens):
+        return [stemmer.stem(t) for t in tokens]
+
+    def preprocess(self, text):
+        tokens = self.remove_punctuation(self.tokenize(self.normalize(text)))
+        stems = self.stem(self.remove_stop_words(tokens))
+        return self.filter_tokens(stems)
 
 
 if __name__ == "__main__":
@@ -66,7 +64,3 @@ if __name__ == "__main__":
     # ts = preprocess(s)
     # print(ts)
     # print(frequency_table(ts, 3))
-
-
-
-
