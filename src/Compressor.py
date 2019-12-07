8c36575f0a6c2649570416d662166eac0192e6f0
aminrakhsha <a.rakhsha144@gmail.com>
2019-12-07 19:21:42 +0330

    ADD compressor file

---
+++ src/Compressor.py
@@ -0,0 +1,19 @@
+def compress_number_gamma(previous, number):
+    pass
+
+
+def compress_number_vl(previous, number):
+    pass
+
+
+def compress_number(previous, number, method):
+    pass
+
+
+def compress_index(index):
+    pass
+
+
+def decompress_index(index):
+    pass
+
