943c1bff415eaa157203c01ea4ecf670119e286f
aminrakhsha <a.rakhsha144@gmail.com>
2019-12-07 16:33:15 +0330

    ADD add_position to Term

--- src/Term.py
+++ src/Term.py
@@ -5,6 +5,9 @@ class Term:
     def get_idf(self, total_count):
         pass
 
+    def add_position(self, doc, location):
+        pass
+
     def __init__(self, str, postings = []):
         self.str = str
         self.postings = postings
