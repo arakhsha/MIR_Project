005754d755c1f5a058a9cbe17f76df0b415a0335
aminrakhsha <a.rakhsha144@gmail.com>
2019-12-22 17:02:59 +0330

    ADD Naive Bayesian Classifier

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,10 +1,15 @@
 from abc import abstractmethod
+from math import log
+
+from data_extaction import read_docs
+from positional_indexing import PositionalIndexer
+from preprocess import EnglishPreprocessor
 
 
 class Classifier:
 
     @abstractmethod
-    def train(self, docs):
+    def train(self, docs, index):
         pass
 
     @abstractmethod
@@ -14,16 +19,49 @@ class Classifier:
 
 class NBClassifier(Classifier):
 
-    def train(self, docs):
-        pass
+    def train(self, docs, index):
+        self.docs = docs
+        self.index = index
+        self.tag_wordcount = {}
+        self.tag_doccount = {}
+        for doc in docs.values():
+            tag = doc.tag
+            if tag not in self.tag_doccount:
+                self.tag_doccount[tag] = 0
+            if tag not in self.tag_wordcount:
+                self.tag_wordcount[tag] = 0
+            self.tag_wordcount[tag] += len(doc.words)
+            self.tag_doccount[tag] += 1
+        self.prior = {}
+        for tag in self.tag_doccount:
+            self.prior[tag] = log(self.tag_doccount[tag] / len(docs.keys()))
+
+    def calc_score(self, tag, term):
+        term_tagcount = 0
+        if term in self.index:
+            for posting in self.index[term].postings:
+                if self.docs[posting.doc_id].tag == tag:
+                    term_tagcount += len(posting.positions)
+            return log((term_tagcount + 1) / (self.tag_wordcount[tag] + len(index.keys())))
+        return log(1 / (self.tag_wordcount[tag] + len(index.keys()) + 1))
+
 
     def classify(self, docs):
-        pass
+        results = {}
+        for doc in docs.values():
+            scores = []
+            for tag in self.prior:
+                score = self.prior[tag]
+                for word in doc.words:
+                    score += self.calc_score(tag, word)
+                scores.append((tag, score))
+            results[doc.id] = sorted(scores, key=lambda x: -x[1])[0][0]
+        return results
 
 
 class KNNClassifier(Classifier):
 
-    def train(self, docs):
+    def train(self, docs, index):
         pass
 
     def classify(self, docs):
@@ -32,7 +70,7 @@ class KNNClassifier(Classifier):
 
 class SVMClassifier(Classifier):
 
-    def train(self, docs):
+    def train(self, docs, index):
         pass
 
     def classify(self, docs):
@@ -41,7 +79,7 @@ class SVMClassifier(Classifier):
 
 class RFClassifier(Classifier):
 
-    def train(self, docs):
+    def train(self, docs, index):
         pass
 
     def classify(self, docs):
@@ -49,4 +87,27 @@ class RFClassifier(Classifier):
 
 
 if __name__ == "__main__":
-    print("Hi!")
+    train_docs = read_docs('../data/phase2_train.csv')
+    test_docs = read_docs('../data/phase2_test.csv')
+
+    preprocessor = EnglishPreprocessor(train_docs)
+
+    for doc in train_docs.values():
+        doc.words = preprocessor.preprocess(doc.text)
+    for doc in test_docs.values():
+        doc.words = preprocessor.preprocess(doc.text)
+
+    print("Preprocess is done!")
+
+    index = PositionalIndexer(train_docs, 1).index
+    print("Index Created Successfully!")
+
+
+    classifier = NBClassifier()
+    classifier.train(train_docs, index)
+    results = classifier.classify(test_docs)
+
+    for i in range(20):
+        print("Predicted Tag:", results[i])
+        print("Text:", test_docs[i].text)
+
