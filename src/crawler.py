b22be5d39ce648adda16ec32c84820fc752c9e97
aminrakhsha <a.rakhsha144@gmail.com>
2020-01-12 12:13:28 +0330

    Change crawler to use html

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,6 @@
-from urllib import request
+import requests
 import json
+import lxml.html as lh
 
 
 def write_results(output_filename, papers):
@@ -8,13 +9,36 @@ def write_results(output_filename, papers):
     file.close()
 
 
-def summarize_data(full_data):
-    paperId = full_data["paperId"]
-    title = full_data["title"]
-    abstract = full_data["abstract"]
-    year = full_data["year"]
-    authors = [x["name"] for x in full_data["authors"]]
-    references = [x["paperId"] for x in full_data["references"]][0:10]
+def fetch_data(url):
+    print("URL:", url)
+    response = requests.get(url)
+    doc = lh.document_fromstring(response.content)
+
+    paperId = url2id(url)
+    title = list(doc.iterfind('.//meta[@name="citation_title"]'))[0].get("content")
+    abstract = list(doc.iterfind('.//meta[@name="description"]'))[0].get("content")
+    year = list(doc.iterfind('.//meta[@name="citation_publication_date"]'))[0].get("content")
+    authors = [x.get("content") for x in doc.iterfind('.//meta[@name="citation_author"]')]
+    references = []
+    next_in_queue = []
+
+    if len([section for section in doc.iterfind('.//div[@id="references"]')]) > 0:
+        references_urls = []
+        refernces_section = doc.get_element_by_id("references")
+        title_sections = refernces_section.find_class("citation__title")
+        for title_section in title_sections:
+            reference_id = title_section.get("data-heap-paper-id")
+            reference_url = None
+            references_links = [link for link in title_section.iterfind('.//a[@href]')]
+            if len(references_links) > 0:
+                reference_url = "https://www.semanticscholar.org" + references_links[0].get("href")
+            if reference_url is not None:
+                references.append(reference_id)
+                references_urls.append(reference_url)
+
+        references = references[0:10]
+        next_in_queue = references_urls[0:5]
+
     result = {
         "id": paperId,
         "title": title,
@@ -23,14 +47,8 @@ def summarize_data(full_data):
         "authors": authors,
         "references": references
     }
-    return result
 
-
-def fetch_full_data(id):
-    respone = request.urlopen("http://api.semanticscholar.org/v1/paper/" + id)
-    print("URL:", "http://api.semanticscholar.org/v1/paper/" + id)
-    data = json.loads(respone.read().decode())
-    return data
+    return result, next_in_queue
 
 
 def url2id(url):
@@ -51,14 +69,14 @@ def save_progress(queue, papers):
 def continue_progress(queue, papers, paper_count):
     fetched_count = len(papers)
     while fetched_count < paper_count and len(queue) > 0:
-        paperId = queue.pop(0)
+        url = queue.pop(0)
+        paperId = url2id(url)
         if paperId not in papers:
             fetched_count += 1
             print("Fetching Paper ", fetched_count)
-            full_data = fetch_full_data(paperId)
-            summarized = summarize_data(full_data)
-            papers[paperId] = summarized
-            queue += summarized["references"][0:5]
+            data, next_in_queue = fetch_data(url)
+            papers[paperId] = data
+            queue += next_in_queue
             if fetched_count % 25 == 0:
                 print("Saved Progress.")
                 save_progress(queue, papers)
@@ -68,8 +86,7 @@ def continue_progress(queue, papers, paper_count):
 
 def crawl(start_filename, paper_count):
     star_file = open(start_filename, "r")
-    start_urls = star_file.read().split()
-    queue = [url2id(url) for url in start_urls]
+    queue = star_file.read().split()
     papers = {}
     papers = continue_progress(queue, papers, paper_count)
     return papers
@@ -88,17 +105,19 @@ def read_progress(queue_filename, papers_filename):
 
 
 if __name__ == "__main__":
-    # papers = crawl("../data/semantic_scholar/start.txt", 5000)
-    queue, papers = read_progress("../data/semantic_scholar/last_queue.json",
-                                  "../data/semantic_scholar/last_papers.json")
+    papers = crawl("../data/semantic_scholar/start.txt", 5)
+    # queue, papers = read_progress("../data/semantic_scholar/last_queue.json",
+    #                               "../data/semantic_scholar/last_papers.json")
     # print(len(papers))
     # print(list(papers.values())[0:3])
-    papers = continue_progress(queue, papers, 5000)
+    # papers = continue_progress(queue, papers, 5000)
     write_results("../data/semantic_scholar/crawled_papers.json", papers)
 
     # print(papers)
     # url = "https://www.semanticscholar.org/paper/Tackling-Climate-Change-with-Machine-Learning-Rolnick-Donti/998039a4876edc440e0cabb0bc42239b0eb29644"
     # id = url2id(url)
-    # full_data = fetch_full_data(id)
+    # data, next_in_queue = fetch_data(url)
+    # print(data)
+    # print(next_in_queue)
     # summarized = summarize_data(full_data)
     # print(summarized)
